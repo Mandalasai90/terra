29621476566067135c81ac695a95151c31fa3714
Mandalasai90 <126144202+Mandalasai90@users.noreply.github.com>
2023-03-21 11:16:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYCJEVYJ4K"
-secret_key = "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"
+access_key = "AKIAR5U4NRGJZ7MRLAAU"
+secret_key = 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""
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-02f3f602d23f1659d"
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "minicube1"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1a"
   user_data       = <<EOF
@@ -25,9 +25,9 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-02f3f602d23f1659d"
   instance_type   = "t2.micro"
-  key_name        = "rmk8s"
+  key_name        = "eks"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone = "ap-south-1b"
   user_data       = <<EOF
